0e9a0d6ff4c62dfcf25395f6dfdae2167c31d6cd
ADMIN <admin@ADMINs-MacBook-Pro.local>
2026-04-09 11:26:36 +0530

    inint

---
+++ android/app/src/main/kotlin/com/assignment/assignment_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.assignment.assignment_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import sqflite_darwin
+import sqlite3_flutter_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+}
